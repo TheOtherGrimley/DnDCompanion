27e8557e2b7eb5837adc4db3c0b878265ef00163
Adam Grimley <adam@Adams-MacBook-Pro.local>
2019-11-01 09:44:36 +0000

    Spell details now show all detail required at the table, scroll rects are bugged

--- Assets/Scripts/ChangeToSpell.cs
+++ Assets/Scripts/ChangeToSpell.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChangeToSpell : ChangePage
 {
+    public GameObject SpellLevelButton;
+
     public override void Change()
     {
         Page pageController;
@@ -20,9 +23,24 @@ public class ChangeToSpell : ChangePage
 
             SpellDeets s = this.GetComponent<SpellDeets>();
 
-            SpellDetailUtil sdu = temp.GetComponent<SpellDetailUtil>();
+            SpellDetailUtil sdu = temp.GetComponent<SpellDetailUtil>(); //Only one GetC. means better performance
             sdu.Title.text = s.name;
-            sdu.Description.text = s.desc;
+            sdu.Description.text = s.desc.Replace("</p><p>", "\n\n").Replace("<p>","").Replace("</p>", "");
+            sdu.Range.text = s.range;
+            sdu.Ritual.text = s.ritual;
+            sdu.Duration.text = s.duration;
+            sdu.Concentration.text = s.concentration;
+            sdu.Cast.text = s.casting_time;
+            sdu.Level.text = s.level;
+            sdu.Classes.text = s.classes;
+            int MinSpellLevel = int.TryParse(s.level[0].ToString(), out MinSpellLevel) ? int.Parse(s.level[0].ToString()) : 0;
+            for(int i = MinSpellLevel; i < 9; i++)
+            {
+                GameObject sp_level = Instantiate(SpellLevelButton, sdu.SpellButtonsParent);
+                sp_level.name = i.ToString();
+                sp_level.GetComponent<Text>().text = i.ToString();
+            }
+            
 
             BookController.Instance.NextPage();
         }
--- Assets/Scripts/SpellDetailUtil.cs
+++ Assets/Scripts/SpellDetailUtil.cs
@@ -7,4 +7,13 @@ public class SpellDetailUtil : MonoBehaviour
 {
     public Text Title;
     public Text Description;
+    public Text Range;
+    public Text Ritual;
+    public Text Duration;
+    public Text Concentration;
+    public Text Cast;
+    public Text Level;
+    public Text Classes;
+    public Transform SpellButtonsParent;
+
 }
